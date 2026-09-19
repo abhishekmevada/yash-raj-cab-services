d987c37f1a6096d2cf4f93d81a6efa56185a71be
Abhishek <abhishekmevada85@gmail.com>
2026-09-19 16:43:01 +0530

    suc

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,9 +4,9 @@ export default function Loading() {
   return (
     <>
       <div className="loadingContainer">
-        <h1>Yash Raj Cab Services</h1>
-        <h2>Starting the Engine...</h2>
-        <h3>
+        <h1 style={{ textAlign: "center" }}>Yash Raj Cab Services</h1>
+        <h2 style={{ textAlign: "center" }}>Starting the Engine...</h2>
+        <h3 style={{ textAlign: "center" }}>
           <mark>Fasten your seatbelt, we’re almost there.</mark>
         </h3>
         <div className="loadingBox">
